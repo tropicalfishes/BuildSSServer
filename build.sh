3fa02981a91c71b72ac4f737c155beff06d7832f
tropicalfishes <tropicalfishes@users.noreply.github.com>
2018-08-24 17:43:06 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,4 @@
+wget --no-check-certificate -O shadowsocks.sh https://raw.githubusercontent.com/tropicalfishes/BuildSSServer/master/autoshadowsocks.sh
+
+chmod +x shadowsocks.sh
+./shadowsocks.sh 2>&1
